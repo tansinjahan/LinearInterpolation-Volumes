f6d54ed674102ef6b91c582a290cd2cbb3a2d56f
tansinjahan <tansin.jahan@gmail.com>
2019-07-23 17:25:09 -0400

    updated with z vector and model architecture

--- model.py
+++ model.py
@@ -8,19 +8,23 @@ import matplotlib.axes
 from skimage import transform
 
 def encoder(inputs):
-    # encoder
-    # 32 x 32 x 32 x 1   -> 16 x 16 x 16 x 32
-    # 16 x 16 x 16 x 32  ->  8 x 8 x 8 x 16
-    # 8 x 8 x 8 x 16    ->  2 x 2 x 2 x 8
+    # encoder ()
+    # 10 x 32 x 32 x 32 x 1   -> 10 x 16 x 16 x 16 x 32
+    # 10 x 16 x 16 x 16 x 32  ->  10 x 8 x 8 x 8 x 16
+    # 10 x 8 x 8 x 8 x 16    ->  10 x 4 x 4 x 4 x 8
+
     net = lays.conv3d(inputs, 32, [4, 4, 4], stride=2, padding='SAME', trainable=True) #[16,16,16,32]
     net = lays.batch_norm(net, decay=0.999)
     net = lays.conv3d(net, 16, [4, 4, 4], stride=2, padding='SAME', trainable=True) #[8,8,8,16]
     net = lays.batch_norm(net, decay=0.999)
     net = lays.conv3d(net, 8, [4, 4, 4], stride=2, padding='SAME', trainable=True) #[4,4,4,8]
     net = lays.batch_norm(net, decay=0.999)
-    #net = lays.fully_connected(net, 100) # fc = [100]
 
-    #net = lays.flatten(net,100)
+    net = lays.flatten(net)
+    net = tf.layers.dense(net, units= 128, activation=tf.nn.relu)
+
+    #net = tf.layers.dense(net, 4, activation=tf.nn.relu)
+    #net = tf.reshape(net, [512])
     print("this is net", net)
     print("this is shape", tf.shape(net))
 
@@ -28,13 +32,19 @@ def encoder(inputs):
 
 def decoder(inputs):
     # decoder
-    # 2 x 2 x 2 x 8   ->  8 x 8 x 8 x 16
+    # 4 x 4 x 4 x 8   ->  8 x 8 x 8 x 16
     # 8 x 8 x 8 x 16  ->  16 x 16 x 16 x 32
     # 16 x 16 x 16 x 32  ->  32 x 32 x 32 x 1
-    #net = lays.fully_connected(inputs, 512)
-    #net = tf.reshape(net, [4,4,4,8])
-    #net = lays.conv3d_transpose(net, 8, [4, 4, 4], stride=2, padding='SAME', trainable=True)
-    net = lays.conv3d_transpose(inputs, 16, [4, 4, 4], stride=2, padding='SAME', trainable= True)
+    #net = lays.fully_connected(inputs, 1)
+
+    #print("decoder input shape", inputs[1], tf.shape(inputs))
+    # net = lays.conv3d_transpose(net, 8, [4, 4, 4], stride=2, padding='SAME', trainable=True)
+
+    net = tf.layers.dense(inputs, units= 512, activation=tf.nn.relu)
+    net = tf.reshape(net, [-1, 4, 4, 4, 8])
+    print("Here", net, tf.shape(net))
+
+    net = lays.conv3d_transpose(net, 16, [4, 4, 4], stride=2, padding='SAME', trainable= True)
     net = lays.batch_norm(net, decay=0.999)
     net = lays.conv3d_transpose(net, 32, [4, 4, 4], stride=2, padding='SAME', trainable= True)
     net = lays.batch_norm(net, decay=0.999)
--- test_autoencoder.py
+++ test_autoencoder.py
@@ -9,18 +9,25 @@ from mpl_toolkits.mplot3d import Axes3D
 import meshlab_visualize
 import model as md
 import for_plot
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+sys.path.append(ROOT_DIR)
 
-#import theano.tensor as T
+print("root:", ROOT_DIR)
 
 # --------------- Define parameters -----------------------------
 batch_size = 10  # Number of samples in each batch
-epoch_num = 5  # Number of epochs to train the network
+epoch_num = 3  # Number of epochs to train the network
 lr = 0.001  # Learning rate
 OUTPUT_SIZE = 32 # size of the output volume produced by decoder
 INPUT_SIZE = 32 # size of the input volume given to the encoder
 total_train_input = 400 # total input volume
 total_test_input = 20 # input for testing the network [10 volumes]
 step_for_saving_graph = 50
+
 
 def interpolationBetnLatentSpace(z1, z2, save_path):
     # -----------interpolation with formula [new_z = (1 - t) * z1 + t * z2] --------------------------
@@ -38,10 +45,10 @@ def interpolationBetnLatentSpace(z1, z2, save_path):
         saver.restore(sess, save_path)
         variables_names = [s.name for s in tf.trainable_variables()]
         values = sess.run(variables_names)
-        file = open("variables_after_restoring.txt", "a+")
+        file = open("variables_after_restoring.txt", "w+")
         for k, v in zip(variables_names, values):
             file.write("Variables:{}, Shape:{}, Values:{}".format(k, (v.shape), v))
-
+        file.close()
         train_interpol_output = sess.run([ae_outputs], feed_dict={l_space: new_z})
         out = np.reshape(train_interpol_output, (OUTPUT_SIZE, OUTPUT_SIZE, OUTPUT_SIZE)).astype(np.float32)
         for_plot.plot_output(out, OUTPUT_SIZE, t)
@@ -57,20 +64,27 @@ def resize_batch(imgs):
     #   imgs: a numpy array of size [batch_size, 32 X 32 x 32].
     # Returns:
     #   a numpy array of size [batch_size, 32, 32, 32].
+
+    #input_file_dic = {}
     imgs = imgs.reshape((-1, 32, 32, 32, 1))
 
     resized_imgs = np.zeros((imgs.shape[0], 32, 32, 32, 1))
     for i in range(imgs.shape[0]):
         resized_imgs[i, ..., 0] = transform.resize(imgs[i, ..., 0], (32, 32, 32))
-    print("This is resized image shape", resized_imgs.shape)
+
+    #print("Input file dictionary info", len(input_file_dic.keys()), len(input_file_dic.values()))
+    #print("This is resized image shape", resized_imgs.shape, resized_imgs)
+    #dic_evaluation(input_file_dic)
     return resized_imgs
 
 
 def loadfile():
+
     input_file = np.array([])
     for i in range(1, (total_train_input + 1)):
-        v = np.loadtxt('/home/gigl/Research/simple_autoencoder/train_data/MyTestFile' + str(i) + '.txt')
+        v = np.loadtxt(ROOT_DIR + '/simple_autoencoder/train_data/MyTestFile' + str(i) + '.txt')
         image_matrix = np.reshape(v, (32, 32, 32)).astype(np.float32)
+
         ax_z, ax_x, ax_y = image_matrix.nonzero()
         input_fig = plt.figure()
         ax = input_fig.add_subplot(111, projection='3d')
@@ -78,14 +92,20 @@ def loadfile():
         plt.savefig('input_data/demo' + str(i) + '.png')
         plt.close()
         input_file = np.append(input_file, image_matrix)
+        
 
     input_file = np.reshape(input_file, (total_train_input, 32 * 32 * 32)).astype(np.float32)
     return input_file
+
+def dic_evaluation(input_dic):
+    for k, v in input_dic.items():
+        print("This is volume:{} and it's values:{}".format(k, v))
 
 
 def next_batch(next_batch_array, batchsize, offset):
     rowStart = offset * batchsize
     rowEnd = (rowStart + batchsize)
+
     return next_batch_array[rowStart:rowEnd, :]
 
 # -------- loss functions from Generative and Discriminative VOXEL modeling paper ------
@@ -96,14 +116,13 @@ def weighted_binary_crossentropy(output, target):
 
 # --------------------read data set----------------------
 input_file = loadfile()  # load 400 chairs as volume with shape [400,32768]
-print("input file shape:", input_file.shape, input_file[0])
-tmp = np.reshape(input_file[0], (32,32,32))
-for_plot.plot_output(tmp, OUTPUT_SIZE, "inputObjCheck")
+
 
 # ----------------- calculate the number of batches per epoch --------------------
-batch_per_ep = input_file.shape[0] // batch_size  # batch per epoch will be 10 [input total= 400 divided by batch-size = 10 ]
+batch_per_ep = input_file.shape[0] // batch_size  # batch per epoch will be 40 [input total= 400 / 10 ]
 
-ae_inputs = tf.placeholder(tf.float32, (None, 32, 32, 32, 1), name="encoder_input")  # input to the network (shape volumes)
+ae_inputs = tf.placeholder(tf.float32, (None, 32, 32, 32, 1), name="encoder_input")  # input to the network
+#dicForShape = tf.placeholder(tf.string, shape=None, name="volume_name")
 
 # ---------for variational auto encoder(this has to be commented when simple auto encoder model is used) --------------
 #z_mean, z_std, l_space = md.encoder(ae_inputs)
@@ -146,6 +165,7 @@ kl_div = tf.constant([1.0])
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
+    file = open("Z_for_trainingShapes.txt", "w+")
     sess.run(init)
     saver = tf.train.Saver()
     plot_loss = np.zeros([1, 4])
@@ -154,16 +174,25 @@ with tf.Session() as sess:
         next_batch_array = input_file  # copy of input file to use for fetching next batch from input array
         for batch_n in range(batch_per_ep):  # batches loop
             batch_img = next_batch(next_batch_array, batch_size, batch_n)  # read a batch
+
             batch_img = resize_batch(batch_img)  # reshape the images to (32, 32, 32)
-            _, c, v_loss, k_loss = sess.run([optimizer, loss, bce_loss, kl_div], feed_dict={ae_inputs: batch_img})
+
+            _, z_vec_to_save, c, v_loss, k_loss = sess.run([optimizer, l_space, loss, bce_loss, kl_div], feed_dict={ae_inputs: batch_img})
+            for i in range (0, z_vec_to_save.shape[0]):
+                #file.write("for batch:{}, Z_vector:{} with shape:{}".format(batch_img.shape, z_vec_to_save, z_vec_to_save.shape))
+                file.write("for image:{} of batch:{} Z_vector:{} with shape:{} \n".format(i,batch_img.shape, z_vec_to_save[i:, :], z_vec_to_save.shape))
+                break
             print('Epoch: {} - cost= {:.5f}'.format((ep + 1), c))
             plot_loss = np.append(plot_loss, [[(ep+1), c, v_loss, k_loss]], axis=0)
 
-    save_path = saver.save(sess, '/home/gigl/Research/simple_autoencoder/checkpoints/model.ckpt')
+    file.close()
+    if not os.path.exists('checkpoints'):
+        os.mkdir('checkpoints')
+    save_path = saver.save(sess, ROOT_DIR + '/simple_autoencoder/checkpoints/model.ckpt')
     print("the model checkpoints save path is %s" % save_path)
     variables_names = [v.name for v in tf.trainable_variables()]
     values = sess.run(variables_names)
-    file = open("variables_after_training.txt", "a+")
+    file = open("variables_after_training.txt", "w+")
     for k, v in zip(variables_names, values):
         file.write("Variables:{}, Shape:{}, Values:{}".format(k, (v.shape), v))
 
@@ -183,7 +212,7 @@ with tf.Session() as sess:
     # ------------------test the trained network for test shapes -------------------------------
     plot_loss_test = np.zeros([1, 2])
     for i in range(1, (total_test_input + 1)):
-        temp = np.loadtxt('/home/gigl/Research/simple_autoencoder/test_data/TestFile' + str(i) + '.txt')
+        temp = np.loadtxt(ROOT_DIR + '/simple_autoencoder/train_data' + str(i) + '.txt')
         test_img = np.reshape(temp, (32, 32, 32)).astype(np.float32)
         ax_z, ax_x, ax_y = test_img.nonzero()
         input_fig = plt.figure()
@@ -211,15 +240,18 @@ with tf.Session() as sess:
     train_shape_1 = resize_batch(input_file[0, :])
     train_shape_2 = resize_batch(input_file[1, :])
     train_l_space1, train_output_image1 = sess.run([l_space, ae_outputs], feed_dict={ae_inputs: train_shape_1})
+    print("lspace shape", train_l_space1.shape, train_output_image1.shape)
     train_output_image1 = np.reshape(train_output_image1, (OUTPUT_SIZE, OUTPUT_SIZE, OUTPUT_SIZE)).astype(np.float32)
     for_plot.plot_output(train_output_image1, OUTPUT_SIZE, 'trainimg')
     train_l_space2, train_output_image2 = sess.run([l_space, ae_outputs], feed_dict={ae_inputs: train_shape_2})
+    print("lspace", train_l_space2.shape)
+    print(train_l_space2)
     start = timer()
     #print("This is the output shape of decoder before interpolation", train_output_image1)
 
     new_z = interpolationBetnLatentSpace(train_l_space1, train_l_space2, save_path)
-    print("This is the shape before interpolation", train_output_image1[1])
+    #print("This is the shape before interpolation", train_output_image1[1])
     interpolation_time = timer() - start
-    print("Interpolation took %f seconds:", interpolation_time)
-    print("This is the shape of train_l_space1", train_l_space1.shape)
+    #print("Interpolation took %f seconds:", interpolation_time)
+    #print("This is the shape of train_l_space1", train_l_space1.shape)
     meshlab_visualize.meshlab_output()
